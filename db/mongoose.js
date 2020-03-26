f6d11c94a692637b44bde01e2ce6262957fb0f68
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-03-26 20:07:13 +0000

    CI - Update

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,11 @@
+/* eslint-disable quotes */
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 require('dotenv').config();
 
 mongoose
 	.connect(
-		process.env.MONGO_DB_HARMONEY_STRING, { useUnifiedTopology: true, useFindAndModify: false, useNewUrlParser: true, useCreateIndex: true })
+		`mongodb+srv://${process.env.HARMONEY_ATLAS_NAME}:${process.env.HARMONEY_ATLAS_PASSWORD}@harmoney-server-q4jlk.mongodb.net/${process.env.HARMONEY_DB_NAME}?retryWrites=true&w=majority`, { useUnifiedTopology: true, useFindAndModify: false, useNewUrlParser: true, useCreateIndex: true })
 	.then(() => console.log('Successfully Connected to [ ' + db.name + ' ]'))
 	.catch(err => console.log('Unable to Connect to [ ' + db.name + ' ]' + ' on all routes', err));
 let db = mongoose.connection;
